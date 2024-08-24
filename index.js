c080b1ad2de4671db4b0e3d59e2c191a9d007f63
amin <aminibrahim25@outlook.com>
2024-08-23 23:46:34 -0400

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function Enter() {
-  if (event.key === "Enter") {
-    todo();
+document.body,addEventListener(`keydown`,(event)=>{
+  if(event.key === `Enter`){
+    todo()
   }
-}
+})
+
+
 
 let todolist = JSON.parse(localStorage.getItem("todolist")) || [];
 
@@ -10,26 +12,38 @@ renderToDoList();
 function renderToDoList() {
   let todolistHTML =  "";
   
-  for (let i = 0; i < todolist.length; i++) {
-    const todoObject = todolist[i];
+  todolist.forEach( (todoObject,index) => {
     let name = todoObject.name;
     let dueDate = todoObject.dueDate;
     let html = `
     
     <div>${name}</div>
     <div>${dueDate}</div>
-    <button class="delete-Button" onclick=" 
-    todolist.splice(${i},1) 
-    renderToDoList()">
-    Delete</button>
+    <button class="delete-Button 
+    js-delete">Delete</button>
     `;
     
     todolistHTML += html;
-  }
-  
+  })
+
   document.querySelector(`.js-para`).innerHTML = todolistHTML;
+
+
+  document.querySelectorAll(`.js-delete`)
+   .forEach( (deleteButton,index) =>{
+    deleteButton.addEventListener('click',()=>{
+      todolist.splice(index,1) 
+      renderToDoList()
+     })
+  })
+    
+  
   localStorage.setItem("todolist", JSON.stringify(todolist))
 }
+
+document.querySelector(`.js-addtodo-button`).addEventListener(`click`,()=>{
+  todo()
+})
 
 
 function todo() {
